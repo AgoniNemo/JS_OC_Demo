3ec9f641a42a960eeed6d41c0e3402db12a348bc
Nemo <XZC168520@163.com>
2016-12-09 18:15:07 +0800

    修改文件

--- JS_OC_Demo/ViewController.m
+++ JS_OC_Demo/ViewController.m
@@ -99,14 +99,14 @@
 }
 -(void)addLocaljscssfileForWebView:(UIWebView *)webView{
 
+     //如果文件类型为 .js ,则创建 script 标签，并设置相应属性，如果文件类型为 .css ,则创建 script 标签，并设置相应属性
+    
     NSString *jsString =@"function loadjscssfile(filename, filetype){\
-    //如果文件类型为 .js ,则创建 script 标签，并设置相应属性\
         if (filetype==\"js\"){\
             var fileref=document.createElement('script');\
             fileref.setAttribute(\"type\",\"text/javascript\");\
             fileref.setAttribute(\"src\", filename);\
         }\
-        //如果文件类型为 .css ,则创建 script 标签，并设置相应属性\
         else if (filetype==\"css\"){\
             var fileref=document.createElement(\"link\");\
             fileref.setAttribute(\"rel\", \"stylesheet\");\
@@ -126,12 +126,9 @@
 -(void)removeWithWebView:(UIWebView *)webView{
 
     NSString *jsString =@"function removejscssfile(filename, filetype){\
-    //判断文件类型\
     var targetelement=(filetype==\"js\")? \"script\" : (filetype==\"css\")? \"link\" : \"none\";\
-    //判断文件名\
     var targetattr=(filetype==\"js\")? \"src\" : (filetype==\"css\")? \"href\" : \"none\";\
     var allsuspects=document.getElementsByTagName(targetelement);\
-    //遍历元素， 并删除匹配的元素\
     for (var i=allsuspects.length; i>=0; i--){\
         if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)\
             allsuspects[i].parentNode.removeChild(allsuspects[i]);\
